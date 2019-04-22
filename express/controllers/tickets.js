e553a0074ac73635b0516754a7cfd9e75fa2a190
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-04-22 01:26:32 -0500

    controllers and routes complete

--- express/controllers/tickets.js
+++ express/controllers/tickets.js
@@ -1,15 +1,36 @@
-var mongoose = require('mongoose')
-var ObjectId = mongoose.Types.ObjectId
-const { TicketsModel } = require('../../mongo/models')
+var mongoose = require("mongoose");
+var ObjectId = mongoose.Types.ObjectId;
+const { TicketsModel } = require("../../mongo/models");
 
 // 1. create a function called getAll that returns everything
+const getAll = () => {
+  return TicketsModel.find({});
+};
 
 // 2. create a function called createMovie that accepts a "movie" param
+const createMovie = movie => {
+  // insert movie from POST request into db
+  return TicketsModel.create({
+    movieName: movie.movieName,
+    description: movie.description,
+    price: movie.price
+  });
+};
 
 // 3. create a function called deleteMovie that accepts a "movieName" param
+const deleteMovie = _movieName => {
+  return TicketsModel.findOneAndDelete({ movieName: _movieName });
+};
 
 // 4. create a function called getById that accepts an "id" param and finds one ticket
 // hint: in your db query, you will use objectId like this: ObjectId(id)
+const getById = id => {
+  return TicketsModel.findOne({ _id: ObjectId(id) });
+};
 
-
-module.exports = {}
+module.exports = {
+  getAll,
+  createMovie,
+  deleteMovie,
+  getById
+};
--- express/routes/tickets.js
+++ express/routes/tickets.js
@@ -1,20 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const TicketsController = require('../controllers/tickets')
+const express = require("express");
+const router = express.Router();
+const TicketsController = require("../controllers/tickets");
 
 // Create a route for getting all movies/tickets from the db
 // This corresponds to item 1 in the controller
-
+router.get("/", (request, response) => {
+  TicketsController.getAll().then(results => response.json(results));
+});
 
 // Create a route for creating a movie
 // This corresponds to item 2 in the controller
-
+router.post("/", (request, response) => {
+  TicketsController.createMovie(request.body)
+    .then(res => response.json(res))
+    .catch(err => console.log(err));
+});
 
 // Create a route for deleting ONE movie by it's name
 // This corresponds to item 3 in the controller
-
-
+router.delete("/:movieName", (request, response) => {
+  TicketsController.deleteMovie(request.params.movieName).then(res =>
+    response.json(res)
+  );
+});
 // Create a route for getting ONE movie by it's id
 // This corresponds to item 4 in the controller
+router.get("/:_id", (request, response) => {
+  TicketsController.getById(request.params._id).then(res => response.json(res));
+});
 
-module.exports = router
+module.exports = router;
